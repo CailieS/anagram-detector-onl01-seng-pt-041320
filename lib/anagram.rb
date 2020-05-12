e0d489aa0b60b6cc6426fb06ab0c0d2a222f17ee
Cailie Skelton <cskelton707@gmail.com>
2020-05-12 17:57:55 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,7 +3,7 @@ class Anagram
   def initialize(word)
     @word = word
   end
-  def match 
+  def match(word_array)
     word_array.select do |word|
     word.split("").sort == @word.split("").sort
   end
